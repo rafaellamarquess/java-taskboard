948675fb7595502587859dd49c7e50502e607b50
Rafaella <fariassrafaella@gmail.com>
2025-03-14 22:30:54 -0300

    initial commit

---
+++ src/main/java/com/example/task_board/persistence/entity/BlockEntity.java
@@ -0,0 +1,17 @@
+package com.example.task_board.persistence.entity;
+
+
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+public class BlockEntity {
+
+    private Long id;
+    private OffsetDateTime blockedAt;
+    private String blockReason;
+    private OffsetDateTime unblockedAt;
+    private String unblockReason;
+
+}
